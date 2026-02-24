7d8d6c1e4898a1e72674c2e1c97a62f0d6aedf29
Geoff Hollingworth <eusholli@gmail.com>
2026-02-24 15:45:50 -0600

    Allow user role to add attendees and meetings

--- lib/role-utils.ts
+++ lib/role-utils.ts
@@ -4,3 +4,7 @@ import { Roles } from './constants'
 export const hasWriteAccess = (role?: string) => {
     return role === Roles.Root || role === Roles.Admin
 }
+
+export const hasCreateAccess = (role?: string) => {
+    return role === Roles.Root || role === Roles.Admin || role === Roles.User
+}
